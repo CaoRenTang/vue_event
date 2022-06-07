b98846ce28e578e17a7faf306dd0c6041046996f
晨曦 <c15293452071@163.com>
2022-06-07 11:58:39 +0800

    发表文章页面构建及富文本编辑器的组件使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import '@/assets/global.less'
 import '@/elementUI' // 注册elementUI组件
+import '@/editor' // 注册富文本编辑器
 
 Vue.config.productionTip = false
 
--- src/views/article/artList.vue
+++ src/views/article/artList.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
+    <!--    内容区域-->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>文章列表</span>
       </div>
       <!-- 搜索区域 -->
       <div class="search-box">
-        <el-form :inline="true" :model="q">
+        <el-form
+          :inline="true"
+          :model="q">
           <el-form-item label="文章分类">
             <el-select v-model="q.cate_id" placeholder="请选择分类" size="small">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+              <el-option v-for="obj in cateList" :key="obj.id" :label="obj.cate_name" :value="obj.cate_id"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="发布状态" style="margin-left: 15px;">
@@ -20,22 +22,60 @@
             </el-select>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" size="small">筛选</el-button>
-            <el-button type="info" size="small">重置</el-button>
+            <el-button size="small" type="primary">筛选</el-button>
+            <el-button size="small" type="info">重置</el-button>
           </el-form-item>
         </el-form>
         <!-- 发表文章的按钮 -->
-        <el-button type="primary" size="small" class="btn-pub">发表文章</el-button>
+        <el-button class="btn-pub" size="small" type="primary" @click="showPubDialogFn">发表文章</el-button>
+        <!-- 发表文章的 Dialog 对话框 -->
+        <el-dialog :before-close="handleClose" :visible.sync="pubDialogVisible" fullscreen title="发表文章">
+          <!-- 发布文章的对话框 -->
+          <el-form :model="pubForm" :rules="pubFormRules" ref="pubFormRef" label-width="100px">
+            <el-form-item label="文章标题" prop="title">
+              <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
+            </el-form-item>
+            <el-form-item label="文章分类" prop="cate_id">
+              <el-select v-model="pubForm.cate_id" placeholder="请选择分类" style="width: 100%;">
+                <el-option v-for="obj in cateList" :key="obj.id" :label="obj.cate_name"
+                           :value="obj.cate_id"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
       </div>
-
       <!-- 文章表格区域 -->
-
       <!-- 分页区域 -->
     </el-card>
+    <!-- 发表文章的 Dialog 对话框 -->
+    <el-dialog
+      :before-close="handleClose"
+      :visible.sync="pubDialogVisible"
+      fullscreen
+      title="发表文章">
+      <!-- 发布文章的对话框 -->
+      <el-form :model="pubForm" :rules="pubFormRules" ref="pubFormRef" label-width="100px">
+        <el-form-item label="文章标题" prop="title">
+          <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
+        </el-form-item>
+        <el-form-item label="文章分类" prop="cate_id">
+          <el-select v-model="pubForm.cate_id" placeholder="请选择分类" style="width: 100%;">
+            <el-option v-for="obj in cateList" :key="obj.id" :label="obj.cate_name" :value="obj.cate_id"></el-option>
+          </el-select>
+        </el-form-item>
+        <!--        富文本编辑器-->
+        <el-form-item label="文章内容" prop="content">
+          <!-- 使用 v-model 进行双向的数据绑定 -->
+          <quill-editor v-model="pubForm.content"></quill-editor>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getArtCateListAPI } from '@/api'
+
 export default {
   name: 'ArtList',
   data () {
@@ -46,8 +86,57 @@ export default {
         pagesize: 2,
         cate_id: '',
         state: ''
+      },
+      // 控制对话框显示隐藏
+      pubDialogVisible: false,
+      // 表单的数据对象
+      pubForm: {
+        title: '', // 文章标题
+        cate_id: '', // 分类id
+        content: '' // 文章内容
+      },
+      // 表单的验证规则对象
+      pubFormRules: {
+        title: [
+          { required: true, message: '请输入文章标题', trigger: 'blur' },
+          { min: 1, max: 30, message: '文章标题的长度为1-30个字符', trigger: 'blur' }
+        ],
+        cate_id: [{ required: true, message: '请选择文章标题', trigger: 'blur' }],
+        content: [{ required: true, message: '请输入文章内容', trigger: 'blur' }]
+      },
+      // 保存分类表单数据
+      cateList: []
+    }
+  },
+  created () {
+    this.getCateListFn()
+  },
+  methods: {
+    // 对话框关闭前的回调 发表文章
+    async handleClose (done) { // done调用放行，关闭对话框
+      // 询问用户是否确认关闭对话框
+      const confirmResult = await this.$confirm('此操作将导致文章信息丢失, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      // 取消了关闭-阻止住, 什么都不干
+      if (confirmResult === 'cancel') return
+      // 确认关闭
+      done()
+    },
+    // 发表文章点击事件
+    showPubDialogFn () {
+      this.pubDialogVisible = true
+    },
+    async getCateListFn () {
+      const { data: res } = await getArtCateListAPI()
+      if (res.code === 0) {
+        this.cateList = res.data
+        console.log(this.cateList)
       }
     }
+
   }
 }
 </script>
@@ -61,5 +150,9 @@ export default {
   .btn-pub {
     margin-top: 5px;
   }
+}
+
+::v-deep .ql-editor {
+  min-height: 300px;
 }
 </style>
